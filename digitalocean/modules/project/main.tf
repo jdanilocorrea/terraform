c86ed11c22ee6098e3a09dd73127653cba7fa0fd
JD Correa Tavares <46714652+jdanilocorrea@users.noreply.github.com>
2025-10-13 15:02:35 -0300

    Update main.tf

--- digitalocean/modules/project/main.tf
+++ digitalocean/modules/project/main.tf
@@ -13,18 +13,3 @@ resource "digitalocean_project_resources" "this" {
   project   = var.project_id
   resources = var.resource_urns
 }
-
-
-# resource "digitalocean_project_resources" "project" {
-#   project = data.digitalocean_project.selected.id
-
-#   resources = [
-#     digitalocean_droplet.this.urn,
-#     digitalocean_firewall.this.urn
-#   ]
-
-#   depends_on = [
-#     digitalocean_droplet.this,
-#     digitalocean_firewall.this
-#   ]
-# }
